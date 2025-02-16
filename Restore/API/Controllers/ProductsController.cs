2a935e856aae5635609d6ee0201b6fe6fc9c4ab7
Tafkas <sven.roels@icloud.com>
2025-02-16 10:46:48 +0100

    End of section 2

---
+++ Restore/API/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController(StoreContext context) : ControllerBase()
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await context.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await context.Products.FindAsync(id); 
+            // beter voor read?: var entity = context.MyEntities.AsNoTracking().FirstOrDefault(e => e.Id == id);
+
+            if (product == null) return NotFound();
+
+            return product;
+        }
+    }
+}

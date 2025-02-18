2faf75b15ace539a9fec5b2bb3ce580b948dfb63
Tafkas <sven.roels@icloud.com>
2025-02-18 14:33:46 +0100

    End of section 7

--- API/Program.cs
+++ API/Program.cs
@@ -19,7 +19,11 @@ var app = builder.Build();
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(opt =>
 {
-    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:3000");
+    opt
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowCredentials() // to allow cookies
+    .WithOrigins("https://localhost:3000");
 });
 
 app.MapControllers();
